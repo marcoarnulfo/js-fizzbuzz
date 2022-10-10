11eccddd453e68434bf83a98cb44840ea67f3763
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-10 17:22:07 +0200

    fix js

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,19 +7,22 @@ for (let index = 1; index < 101; index++) {
     const li = document.createElement("li")
     li.append(index)
     listElement.append(li)
+
+    if (index % 3 == 0 && index % 5 == 0) {
+        li.innerHTML = "FizzBuzz"
+        console.log("FizzBuzz")
+    }
     // SE multipli di 3, stampa Fizz
-    if (index % 3 === 0) {
+    else if (index % 3 === 0) {
         console.log("Fizz")
         li.innerHTML = "Fizz"
     }
     // ALTRIMENTI SE multipli di 5, stampa Buzz
-    if (index % 5 === 0) {
+    else if (index % 5 === 0) {
         console.log("Buzz")
         li.innerHTML = "Buzz"
     }
     // ALTRIMENTI  per i numeri sia multipli di 3 e di 5, stampi FizzBuzz
-    if (index % 3 == 0 && index % 5 == 0) {
-        li.innerHTML = "FizzBuzz"
-    }
+    
 }
 
