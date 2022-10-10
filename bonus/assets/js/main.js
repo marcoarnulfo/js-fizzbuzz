ef110cf60197e91495ef651ac24d44c14c7984dd
Marco Arnulfo <arnulfomarco7@gmail.com>
2022-10-10 17:31:38 +0200

    fix bonus folder

--- bonus/assets/js/main.js
+++ bonus/assets/js/main.js
@@ -1,32 +1,28 @@
-console.log("hello");
 
 const listElement = document.getElementById("list")
 
 //Scrivi un programma che stampi in console i numeri da 1 a 100,
-
 for (let index = 1; index < 101; index++) {
     console.log(index);
     const li = document.createElement("li")
     li.append(index)
     listElement.append(li)
+
+    if (index % 3 == 0 && index % 5 == 0) {
+        li.innerHTML = "FizzBuzz"
+        console.log("FizzBuzz")
+    }
     // SE multipli di 3, stampa Fizz
-    if (index % 3 === 0) {
-        //console.log("Fizz")
+    else if (index % 3 === 0) {
+        console.log("Fizz")
         li.innerHTML = "Fizz"
     }
     // ALTRIMENTI SE multipli di 5, stampa Buzz
-    if (index % 5 === 0) {
-        //console.log("Buzz")
+    else if (index % 5 === 0) {
+        console.log("Buzz")
         li.innerHTML = "Buzz"
     }
     // ALTRIMENTI  per i numeri sia multipli di 3 e di 5, stampi FizzBuzz
-    if (index % 3 == 0 && index % 5 == 0) {
-        li.innerHTML = "FizzBuzz"
-    }
+    
 }
-
-
-// && (x <4 && y >5) x=3 y=4 falso, uno dei due non è vero
-
-// || (x <4 || y >5) x=3 y=4 vero, uno dei due è vero
 
